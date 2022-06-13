3dbe72f488a7ac08aab15e3498a84bac4a0e97bc
Ionela Denisa Pop <ionelapop99@gmail.com>
2022-06-13 18:03:56 +0300

    Create the Recipe Card Control v2

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,47 +1,46 @@
-import {useState} from 'react';
-import {cardStyle,imageSectionStyle,titleStyle,descriptionStyle,detailsStyle,detailElementStyle,paragraphStyle} from './RecipeCardStyle.module.js';
+import { useState, memo } from 'react';
+import { cardStyle, imageSectionStyle, titleStyle, descriptionStyle, detailsStyle, detailContainerStyle, paragraphStyle, imageStyle, headerDetailStyle, bodyDetailStyle } from './RecipeCardStyle.module.js';
 
-const exempleRecipe={
+const exempleRecipe = {
     title: "Shaorma",
-    description: "Sharoma rapida, gustoasa si sanatoasa. Usor de facut, usor de savurat.",
-    preparationTime: 30,
+    description: "Sharoma rapida, gustoasa si sanatoasa. Usor de facut, usor de savurat. Mai multa descriere bing bing bing bing bing bing bing bing bing bing bing bing ",
+    preparationTime: "30 min",
     dificulty: "Basic",
     numberOfIngredients: 10,
-    url:'./images/shaorma.jpg'
-    //https://t3.ftcdn.net/jpg/01/32/05/84/360_F_132058496_aE136Gt4ZP7MC7pzMgyxImJsp4GVmgID.jpg
+    url: 'https://t3.ftcdn.net/jpg/01/32/05/84/360_F_132058496_aE136Gt4ZP7MC7pzMgyxImJsp4GVmgID.jpg',
 }
 
-function RecipeCard(){
-    const [recipe,setRecipe]= useState(exempleRecipe)
+function RecipeCard() {
+    const [recipe, setRecipe] = useState(exempleRecipe)
 
     return (
         <div style={cardStyle}
-            onClick={(e)=> console.log("Redirect the user to the recipe page!")}
+            onClick={(e) => console.log("Redirect the user to the recipe page!")}
         >
             <div style={imageSectionStyle}>
-                <img src={recipe.url} alt={recipe.title} />
+                <img src={recipe.url} alt={recipe.title} style={imageStyle} />
             </div>
             <div>
                 <h3 style={titleStyle}>{recipe.title}</h3>
                 <p style={descriptionStyle}>{recipe.description}</p>
                 <div style={detailsStyle}>
-                    <div style={detailElementStyle}>
-                        <p style={paragraphStyle}>Time</p>
-                        <p style={paragraphStyle}>{recipe.preparationTime}</p>
+                    <div style={detailContainerStyle}>
+                        <p style={{ ...paragraphStyle, ...headerDetailStyle }}>Time</p>
+                        <p style={{ ...paragraphStyle, ...bodyDetailStyle }}>{recipe.preparationTime}</p>
                     </div>
-                    <div style={detailElementStyle}>
-                        <p style={paragraphStyle}>Difficulty</p>
-                        <p style={paragraphStyle}>{recipe.dificulty}</p>
+                    <div style={detailContainerStyle}>
+                        <p style={{ ...paragraphStyle, ...headerDetailStyle }}>Difficulty</p>
+                        <p style={{ ...paragraphStyle, ...bodyDetailStyle }}>{recipe.dificulty}</p>
                     </div>
-                    <div style={detailElementStyle}>
-                        <p style={paragraphStyle}>Ingredients</p>
-                        <p style={paragraphStyle}>{recipe.numberOfIngredients}</p>
+                    <div style={detailContainerStyle}>
+                        <p style={{ ...paragraphStyle, ...headerDetailStyle }}>Ingredients</p>
+                        <p style={{ ...paragraphStyle, ...bodyDetailStyle }}>{recipe.numberOfIngredients}</p>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     )
 }
 
-export default RecipeCard;
+export default memo(RecipeCard);
